645529c5e50108735e69ca9c902597bf1a19fefa
Syonder <24651896+Syonder@users.noreply.github.com>
2020-11-04 22:03:52 +0100

    Fill url and README

---
+++ README.md
@@ -0,0 +1,5 @@
+# The Game env
+
+This project is basic core to simulate "The Game" by Steffen Benndorf.
+
+Check rules here : https://nsv.de/wp-content/uploads/2018/05/the-game-english.pdf
--- the_game/__init__.py
+++ the_game/__init__.py
@@ -9,7 +9,7 @@ __description__ = "Core package to simulate 'The Game' game."
 __author__ = "Syonder"
 __version__ = "0.0.1"
 __license__ = "Apache-2.0"
-__url__ = "CHANGE-ME"
+__url__ = "https://github.com/Syonder/TheGameEnv.git"
 
 __all__ = [
     "GameEnv",
