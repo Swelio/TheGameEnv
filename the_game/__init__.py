32b82bf756a442a2480fcc54043304d6db610d59
Swelio <24651896+Swelio@users.noreply.github.com>
2022-01-11 13:30:25 +0100

    Update __init__.py
    Update author's name

--- the_game/__init__.py
+++ the_game/__init__.py
@@ -6,10 +6,10 @@ from the_game.game_env import GameEnv, Heap, Player
 
 __package_name__ = "the_game"
 __description__ = "Core package to simulate 'The Game' game."
-__author__ = "Syonder"
+__author__ = "Swelio"
 __version__ = "0.0.1"
 __license__ = "Apache-2.0"
-__url__ = "https://github.com/Syonder/TheGameEnv.git"
+__url__ = "https://github.com/Swelio/TheGameEnv.git"
 
 __all__ = [
     "GameEnv",
